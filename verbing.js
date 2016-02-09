be310f144ff71322763436830835bcec9924973a
ebonertz <bonertz.unc@gmail.com>
2016-02-09 10:42:07 -0500

    Add verbing.js

---
+++ verbing.js
@@ -0,0 +1,14 @@
+<doctype! JavaScript>
+
+
+var value = X
+
+function verbing (value) {
+  if verbing.length >= 3:
+  value.push("ing")
+}
+if value.length <= 3
+print (value)
+
+if value //ends in ing
+value.push("ly")
