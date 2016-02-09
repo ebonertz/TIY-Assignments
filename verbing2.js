df31a5b407d2d50da670aacb79f0ca3c690d9f1e
ebonertz <bonertz.unc@gmail.com>
2016-02-09 11:10:40 -0500

    Added verbing2.js

---
+++ verbing2.js
@@ -0,0 +1,11 @@
+<doctype! JavaScript>
+function verbing (value) {
+    if (value.length < 3) return word;
+    if (value.slice(-3) === 'ing') {
+     return value + ly;
+ }  else {
+     return value + 'ing';
+     }
+}
+
+console.log(verbing("skank"))
