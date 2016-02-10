7164d5c829e4ecfed25e4214d52127fad0073828
ebonertz <bonertz.unc@gmail.com>
2016-02-10 10:44:32 -0500

    Inital commit for recipe.js

---
+++ recipe.js
@@ -0,0 +1,10 @@
+<doctype! JavaScript>
+
+var recipe = {
+    title : "Pancakes",
+    serves : 4,
+    ingredients : ["Bisquick ", "Eggs ", "Milk "],
+};
+console.log(recipe.title);
+console.log("Serves: "+ recipe.serves);
+console.log("Ingredients: " + recipe.ingredients);
