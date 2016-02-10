7b64e84b53961ea9b049dc0150c443a99c0ebc15
ebonertz <bonertz.unc@gmail.com>
2016-02-10 09:59:46 -0500

    add agecalculator

---
+++ agecalculator.js
@@ -0,0 +1,9 @@
+<doctype! JavaScript>
+
+    function calculateAge(yourAge) {
+    var currentYear = Date.now() - birthday.getTime();
+    var birthYear = new Date(ageDifMs);
+
+
+    return (You are either yourAge or yourAge - currentYear)   ;
+}
