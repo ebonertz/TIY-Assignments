31445bf6ef8f1aa9da020549ed4402b19865af94
ebonertz <bonertz.unc@gmail.com>
2016-02-11 10:09:50 -0500

    Initial commit for needed

---
+++ needed.js
@@ -0,0 +1,6 @@
+    var age = "26"
+    var amountPerDay = "5"
+    var maxAge = "101"
+    var totalNeeded = (amountPerDay * 365) * (maxAge- age);
+    var message = "You will need " + totalNeeded + " pickles to last you to the ripe old age of " + maxAge
+console.log(message)
