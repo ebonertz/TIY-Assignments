86849491305ceed51e136d27d3a0c29ff7cd947f
ebonertz <bonertz.unc@gmail.com>
2016-02-12 18:59:24 -0500

    Add console functionality

---
+++ Multi-Form/index.js
@@ -0,0 +1,50 @@
+/*  function myFunction() {
+        var x  = document.getElementById("frm1");
+        var text = "";
+        var i;
+      for (i=0; i < x.length ; i++) {
+        text += x.elements[i].value + ""<br>"" ;
+      } */
+
+
+
+var fname = document.querySelector("#first-name");
+var lname = document.querySelector("#last-name");
+var email = document.querySelector("#email-address");
+
+
+  var formSubmit = document.querySelector("button");
+  formSubmit.addEventListener("click", function(event) {
+        console.log(fname.value);
+        event.preventDefault();
+      });
+
+    formSubmit.addEventListener("click", function(event) {
+          console.log(lname.value);
+          event.preventDefault();
+        });
+
+      formSubmit.addEventListener("click", function(event) {
+            console.log(email.value);
+            event.preventDefault();
+          });
+
+
+
+        //var submit= document.getElementById("first-name")
+      //  submit.addEventListener()
+
+
+      /*
+
+
+      onClick.addEventListener("click", function( )    {
+              console.log();
+            });
+
+
+      var myForm= ["#firstName", "#lastName", "#emailAddress"];
+
+            for (var i=0; i<=myForm.length; i++) {
+              console.log(myForm[i])
+            } **/
