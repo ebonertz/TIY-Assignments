3a991e5aae8d891e172d26bc0286bd389fd744b1
ebonertz <bonertz.unc@gmail.com>
2016-02-16 10:44:12 -0500

    Initial Commit

---
+++ pluralize.js
@@ -0,0 +1,11 @@
+function pluralize( noun, number) {
+    var count= (number + noun);
+
+    if (number <= 1) {
+    console.log("count");
+    }
+
+    else (number > 1) {
+    console.log(number+ noun + "s");
+}
+}
