bfb89bade15f06ee7109a957bfba3cf362634d43
ebonertz <bonertz.unc@gmail.com>
2016-02-17 10:34:43 -0500

    Initial commit

---
+++ translateworld.js
@@ -0,0 +1,14 @@
+function helloWorld ( ) {
+    var language = "fun"
+    var en = english
+    var es = spanish
+    var de = german
+
+    if (language= 'en')
+     console.log("Hello, World")
+    else if (language= 'de')
+    console.log("Hallo, Welt")
+    else if (language= 'es')
+    console.log("Hola, Mundo")
+
+}
