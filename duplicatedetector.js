4db02edf0295ed18acfec60aaa81c0d762b3871d
Evan Bonertz <bonertz.unc@gmail.com>
2016-03-17 10:54:01 -0400

    Update duplicatedetector.js

--- duplicatedetector.js
+++ duplicatedetector.js
@@ -1,19 +1,19 @@
 function duplicate (str) {
   var array= str.split('');
   var result = '';
-  var controller = 0;
+  var counter = 0;
 
   for (var x = 0; x < array.length; x++) {
-    controller = 0;
+    counter = 0;
 
     for (var y = 0; y < array.length; y++)
     {
       if (array[x] === array[y]) {
-        controller+= 1;
+        counter+= 1;
       }
     }
 
-    if (controller < 2) {
+    if (counter < 2) {
       result = array[x];
       break;
     }
